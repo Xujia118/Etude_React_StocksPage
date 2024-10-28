d606f98fcbedf01127c61e9ed7dec463a517e325
Jia XU <xujia118@hotmail.com>
2024-10-28 00:28:53 -0700

    Add styling

--- src/AssetList.jsx
+++ src/AssetList.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 function AssetList({ assets, onLoadAssets }) {
-  const columns = ["ticker", "price", "volumn", "sector", "change_percent"];
+  const columns = ["ticker", "price", "volume", "sector", "change_percent"];
   const sectors = [
     { value: "", label: "All Sectors" },
     { value: "automotive", label: "Automotive" },
@@ -74,20 +74,24 @@ function AssetList({ assets, onLoadAssets }) {
   }
 
   return (
-    <>
-      <form className="control-panel">
+    <div className="flex flex-col md:flex-row">
+      <form className="flex-1 p-4 border-r border-gray-300">
         <input
           type="text"
           placeholder="Search a ticker..."
           value={searchQuery}
           onChange={(e) => setSearchQuery(e.target.value)}
+          className="p-2 border border-gray-300 rounded mb-4 w-full"
         />
-        <div>
-          <label htmlFor="select-sector">Select a sector:</label>
+        <div className="mb-4">
+          <label htmlFor="select-sector" className="block mb-1">
+            Select a sector:
+          </label>
           <select
             name="sector"
             id="select-sector"
             onChange={handleFilterSector}
+            className="p-2 border border-gray-300 rounded w-full"
           >
             {sectors.map((sector) => (
               <option key={sector.value} value={sector.value}>
@@ -96,49 +100,47 @@ function AssetList({ assets, onLoadAssets }) {
             ))}
           </select>
         </div>
-
-        {/* TODO */}
-        <div>
-          <label htmlFor="">Price</label>
-        </div>
-        <label htmlFor="">Volumn</label>
-        <label htmlFor="">Change</label>
       </form>
 
-      <ul>
-        <div>
-          {columns.map((column) => {
-            if (column === "ticker") {
-              return null;
-            }
-            return (
-              <button
-                className="button-title"
-                type="button"
-                key={column}
-                onClick={() => handleClick(column)}
-              >
-                {column} {sortOrder[column] ? "↑" : "↓"}
-              </button>
-            );
-          })}
-        </div>
-
-        {(!!filteredAssets.length ? filteredAssets : assets).map(
-          (asset, index) => (
-            <li key={index}>
-              <div>
-                {asset.ticker}
-                {asset.price}
-                {asset.volumn}
-                {asset.sector}
-                {asset.change_percent}
-              </div>
-            </li>
-          )
-        )}
-      </ul>
-    </>
+      <div className="flex-[4] overflow-x-auto">
+        <table className="w-full">
+          <thead>
+            <tr>
+              {columns.map((column) => (
+                <th className="p-2 border-b-2 text-center hover:bg-gray-200 whitespace-nowrap">
+                  <button
+                    className="text-transform: capitalize"
+                    type="button"
+                    key={column}
+                    onClick={() => handleClick(column)}
+                  >
+                    {column} {sortOrder[column] ? "↑" : "↓"}
+                  </button>
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {(!!filteredAssets.length ? filteredAssets : assets).map(
+              (asset, index) => (
+                <tr
+                  key={index}
+                  className={`py-2 ${
+                    index % 2 === 0 ? "bg-white" : "bg-gray-100"
+                  }`}
+                >
+                  {columns.map((column) => (
+                    <td key={column} className="p-2 text-center">
+                      {asset[column]}
+                    </td>
+                  ))}
+                </tr>
+              )
+            )}
+          </tbody>
+        </table>
+      </div>
+    </div>
   );
 }
 
